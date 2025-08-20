6f994601b825556649e7215cb1193f7db9314296
Talish-Khan <talishalikhan99@gmail.com>
2025-08-20 20:26:57 +0530

    Updated the path based on runmodes

--- core/src/main/java/com/adobe/aem/spark/project/core/servlets/PageInfoServlet.java
+++ core/src/main/java/com/adobe/aem/spark/project/core/servlets/PageInfoServlet.java
@@ -6,6 +6,7 @@ import java.util.HashMap;
 import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
+import java.util.Set;
 
 import javax.servlet.Servlet;
 import javax.servlet.ServletException;
@@ -13,69 +14,97 @@ import javax.servlet.ServletException;
 import org.apache.sling.api.SlingHttpServletRequest;
 import org.apache.sling.api.SlingHttpServletResponse;
 import org.apache.sling.api.resource.Resource;
+import org.apache.sling.api.resource.ResourceResolver;
 import org.apache.sling.api.servlets.HttpConstants;
 import org.apache.sling.api.servlets.SlingSafeMethodsServlet;
 import org.apache.sling.servlets.annotations.SlingServletResourceTypes;
+import org.apache.sling.settings.SlingSettingsService;
 import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Reference;
 
+import com.day.cq.commons.Externalizer;
 import com.day.cq.wcm.api.Page;
 import com.day.cq.wcm.api.PageManager;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
 @Component(service = Servlet.class)
 @SlingServletResourceTypes(resourceTypes = { "spark/components/page" },
-methods = {HttpConstants.METHOD_GET},
-selectors = {"info"},
-extensions = {"json"})
+ methods = {HttpConstants.METHOD_GET},
+ selectors = { "info" },
+ extensions = { "json" })
 public class PageInfoServlet extends SlingSafeMethodsServlet {
-
 
     private static final ObjectMapper mapper = new ObjectMapper();
 
+    @Reference
+    private Externalizer externalizer;
+
+    @Reference
+    private SlingSettingsService slingSettingsService;
 
     @Override
     protected void doGet(SlingHttpServletRequest request, SlingHttpServletResponse response)
             throws ServletException, IOException {
-            
-      response.setContentType("text/json");
 
-       Resource resource = request.getResource();
+        response.setContentType("application/json");
+        response.setCharacterEncoding("UTF-8");
 
-       if (resource == null) {
-        
+        Resource resource = request.getResource();
+
+        ResourceResolver resolver = request.getResourceResolver();
+
+        if (resource == null) {
+
             response.setStatus(SlingHttpServletResponse.SC_NOT_FOUND);
-            mapper.writeValue(response.getWriter(), Map.of("error","No Resource Found"));
-       
+            mapper.writeValue(response.getWriter(), Map.of("error", "No Resource Found"));
+
             return;
-        }    
+        }
 
         PageManager pageManager = request.getResourceResolver().adaptTo(PageManager.class);
         Page page = pageManager.getContainingPage(resource);
-        
+
         if (page == null) {
             response.setStatus(SlingHttpServletResponse.SC_NOT_FOUND);
-            mapper.writeValue(response.getWriter(), Map.of("error", "No page found for resource " + resource.getPath()));
+            mapper.writeValue(response.getWriter(),
+                    Map.of("error", "No page found for resource " + resource.getPath()));
             return;
         }
+
+        Set<String> runModes = slingSettingsService.getRunModes();
+        boolean isAuthor = runModes.contains("author");
+
+         String relativeUrl = page.getPath() + ".html";
+
+            String fullUrl;
+
+            if (isAuthor) {
+                fullUrl = externalizer.authorLink(resolver, relativeUrl) + "?wcmmode=disabled";
+            } else {
+                fullUrl = externalizer.publishLink(resolver, relativeUrl);
+            }
 
         Map<String, Object> result = new HashMap<>();
         result.put("Path", page.getPath() + ".html");
         result.put("title", page.getTitle());
         result.put("lastModifiedDate", page.getLastModified());
+        result.put("fullPath", fullUrl);
         // result.put("template", page.getTemplate());
 
         List<Map<String, Object>> childPages = new ArrayList<>();
         Iterator<Page> children = page.listChildren();
 
-        while (children.hasNext() ) {
-             
+        while (children.hasNext()) {
+
             Page child = children.next();
-             Map<String, Object> childInfo = new HashMap<>();
-             childInfo.put("path", child.getPath() + ".html") ;
-             childInfo.put("title", child.getTitle());
-             childInfo.put("lastModifiedDate", child.getLastModified());
-            
-             childPages.add(childInfo);
+           
+            Map<String, Object> childInfo = new HashMap<>();
+            childInfo.put("path", child.getPath() + ".html");
+            childInfo.put("title", child.getTitle());
+            childInfo.put("lastModifiedDate", child.getLastModified());
+            childInfo.put("fullPath", fullUrl);
+
+            childPages.add(childInfo);
 
         }
         result.put("ChildPages", childPages);
@@ -83,6 +112,5 @@ public class PageInfoServlet extends SlingSafeMethodsServlet {
         mapper.writeValue(response.getWriter(), result);
 
     }
-
 
 }
