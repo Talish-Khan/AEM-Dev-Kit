af0ef8589d791e5e7c1822df8849fa47faadb413
Talish-Khan <talishalikhan99@gmail.com>
2026-02-28 16:22:19 +0530

    Tested translation and style

---
+++ core/src/test/java/com/adobe/aem/spark/project/core/services/configs/SiteConfigConfigurationsTest.java
@@ -0,0 +1,20 @@
+package com.adobe.aem.spark.project.core.services.configs;
+
+import org.junit.jupiter.api.Test;
+
+public class SiteConfigConfigurationsTest {
+    @Test
+    void testEmail() {
+
+    }
+
+    @Test
+    void testItemsPerPage() {
+
+    }
+
+    @Test
+    void testSiteName() {
+
+    }
+}
